69fc9ebcb8255bdb6f9790b909cdc1ef6ea8ece3
Pantagruel74 <tostar74@mail.ru>
2022-09-07 11:39:19 +0500

    Create fastDi functions module

---
+++ test-unit/loader.test.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const loader = require(path.resolve(__dirname, '../js/fastDi'));
+const assert = require('assert');
+
+it('test register', () => {
+    assert.equal(!!global.fastDi, false);
+    loader.register();
+    assert.deepEqual(global.fastDi, {
+        "someI": path.resolve(__dirname, "./someFile.js")
+    });
+}); 
+
+it('test load', () => {
+    const someFile = loader.load('someI');
+    assert.equal(someFile.returnAboba(), 'aboba');
+});
