9a93f3102f5488aa7b98f640625f83fe766fda26
Pantagruel74 <tostar74@mail.ru>
2022-09-10 07:48:59 +0500

    clear potential bug with file-names-char-cases.

--- test-unit/somefile2.js
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.returnAboba = void 0;
-
-function returnAboba()
-{
-    return 'aboba';
-}
-exports.returnAboba = returnAboba;
